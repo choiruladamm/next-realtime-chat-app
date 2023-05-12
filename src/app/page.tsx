573f9b2de4d9af929836a09643dd69eb3ce4f9b9
choiruladamm <choiruladamm@gmail.com>
2023-05-12 22:35:42 +0700

    Add Dashboard & Button Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="h-[15rem] border w-[15rem] grid place-items-center">
       <p className="text-yellow-300">Hello World</p>
+      <a href="/dashboard">jknkjd</a>
     </div>
   );
 }
