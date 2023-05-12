af68e0a8f6b57a744c06ebba1205f3415a094e5d
choiruladamm <choiruladamm@gmail.com>
2023-05-13 00:54:54 +0700

    Connect DB @upstash/redis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import { db } from "@/lib/db";
-
-export default async function  Home() {
-  
-  await db.set('hello', 'hello')
-
+export default async function Home() {
   return (
     <main className="h-[15rem] border-slate-900 w-[15rem] grid place-items-center">
       <p className="text-red-600">Hello World</p>
