663c4302e8b07e0175be8a04122355a08d3bd33a
choiruladamm <choiruladamm@gmail.com>
2023-05-14 20:22:53 +0700

    Add Authentication Google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import Button from "@/components/ui/Button";
+
 export default async function Home() {
-  return (
-    <main className="h-[15rem] border-slate-900 w-[15rem] grid place-items-center">
-      <p className="text-red-600">Hello World</p>
-    </main>
-  );
+  return <Button>Hello</Button>;
 }
